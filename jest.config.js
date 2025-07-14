89bda2bd8638c784c304cd9ca7934b4fe61726f9
David Murdoch <dsmurdoch357@gmail.com>
2025-07-14 12:14:24 +0100

    ci: add unit-testing pipeline

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+// https://jestjs.io/docs/configuration
+
+/**
+ * @type {import("jest").Config}
+ */
+const config = {
+  preset: "jest-expo",
+  transformIgnorePatterns: [
+    "node_modules/(?!(?:.pnpm/)?((jest-)?react-native|@react-native(-community)?|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg))",
+  ],
+};
+
+export default config;
