aeb71c1a5e6ef7a221c434b15ded7c5e6f3ec38a
David Murdoch <dsmurdoch357@gmail.com>
2025-07-16 18:37:18 +0100

    feat(settings page): add contents around storage read

--- src/app/settings/index.tsx
+++ src/app/settings/index.tsx
@@ -1,8 +1,32 @@
-import { Stack } from "expo-router";
-import { StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { Link, Stack } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Pressable, StyleSheet } from "react-native";
+import BarcodeScannerIcon from "~/assets/images/barcodeScanner.svg";
+import CheckIcon from "~/assets/images/check.svg";
+import CrossIcon from "~/assets/images/cross.svg";
+import { ExternalLink } from "~/components/ExternalLink";
 import { Text, View } from "~/components/Themed";
 
 const ScannerScreen = () => {
+  const [parkrunId, setParkrunId] = useState<string | null>(null);
+
+  const getParkrunIdFromStorage = async () => {
+    try {
+      const value = await AsyncStorage.getItem("parkrunId");
+
+      if (!value) {
+        setParkrunId(value);
+      }
+    } catch {
+      alert("Failed to fetch the parkrunId from storage");
+    }
+  };
+
+  useEffect(() => {
+    getParkrunIdFromStorage();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Stack.Screen
@@ -18,12 +42,37 @@ const ScannerScreen = () => {
         }}
       />
       <Text style={styles.title}>Scan your parkrun barcode</Text>
+
+      <View style={styles.idContainer}>
+        {parkrunId ? (
+          <>
+            <CheckIcon width={20} height={20} fill="#00ceae" />
+            <Text style={styles.idText}>A1129502</Text>
+            <CrossIcon
+              style={styles.cross}
+              width={20}
+              height={20}
+              color="#9CA3AF"
+            />
+          </>
+        ) : (
+          <Link href="/" style={styles.link} asChild>
+            <Pressable>
+              <BarcodeScannerIcon width={20} height={20} fill="#00ceae" />
+              <Text style={styles.idText}>Scan Now</Text>
+            </Pressable>
+          </Link>
+        )}
+      </View>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <Text>made by David Murdoch</Text>
+      <Text>
+        made by David Murdoch (
+        <ExternalLink href="https://github.com/dsm23">dsm23</ExternalLink>)
+      </Text>
     </View>
   );
 };
@@ -31,13 +80,28 @@ const ScannerScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // alignItems: "center",
-    // justifyContent: "center",
     padding: 32,
+  },
+  idContainer: {
+    alignItems: "center",
+    marginTop: 16,
+    flexDirection: "row",
+  },
+  idText: {
+    marginLeft: 16,
+    color: "#00ceae",
+    fontSize: 18,
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
+  },
+  link: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  cross: {
+    marginLeft: "auto",
   },
   separator: {
     marginVertical: 30,
